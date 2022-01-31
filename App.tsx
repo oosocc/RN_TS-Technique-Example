4809531503adb74a51617ab65c9972b0c26e103c
Hoooooou-Jun <jipkim2@gmail.com>
2022-01-31 22:02:32 +0900

    옵셔널, 기본값, Children Props

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Text } from 'react-native';
 import Profile from './Profile';
 
 const App = () => {
     return (
-        <Profile name="hojun" />
-    );
+        <Profile name="hojun" isActive={true}>
+            <Text>Hello world</Text>
+        </Profile>
+    )
 };
 
 export default App;
--- Profile.tsx
+++ Profile.tsx
@@ -1,16 +1,34 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import React from 'react';
 
 interface Props {
     name: string;
+    isActive?: boolean; // 옵셔널 Props, 생략 가능
+    image: string;
+    children: React.ReactNode; // children props
 }
 
-const App = ({name}: Props) => {
+const Profile = ({name, isActive, image, children}: Props) => {
     return (
-        <View>
+        <View style={isActive && styles.activeStyle}>
+            <Image style={{width: '65%', height: '30%'}} source={{uri: image}} />
             <Text>{name}</Text>
+            <View>{children}</View>
         </View>
     );
 };
 
-export default App;
+Profile.defaultProps = {
+    image: 'https://picsum.photos/200', // props가 없을 시 defaultProps로 설정 가능. interface에 ? 붙여도 되나 둘 다 언젠가 deprecated 될 수 있음.
+}
+
+const styles = StyleSheet.create({
+    activeStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'yellow'
+    }
+})
+
+export default Profile;
